fc114da3dc60a47988eab1193f647c00d425e610
intheon <allobon@gmail.com>
2016-06-20 19:15:41 +0100

    connected to a firebase!

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  headerMessage: "Coming Soon",
+  responseMessage: "",
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.not('isValid')
+  isDisabled: Ember.computed.not('isValid'),
+
+
+  actions: {
+  	saveInvitation() {
+  		const email = this.get("emailAddress");
+
+  		const newInvitation = this.store.createRecord("invitation", {email: email});
+
+  		newInvitation.save().then((response) => {
+	  		this.set("responseMessage", `thanks, i now have the email ${this.get("emailAddress")}`);
+	  		this.set("emailAddress", "");
+  		});
+
+
+
+  	}
+  }
 
 });
