6bd5852dee408f27b4a2ff12f8daf4c131080e7e
Ben S <allobon@gmail.com>
2016-06-20 13:12:19 +0100

    and he we have some basic routes and controllers set up

---
+++ app/controllers/index.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid')
+
+});
